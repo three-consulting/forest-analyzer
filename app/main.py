491a7fefa4b48e557ff25773869858a093b98669
ToukoH <touko@three.consulting>
2023-04-13 13:04:55 +0300

    placeholder text 1

--- app/main.py
+++ app/main.py
@@ -55,8 +55,14 @@ def app():
     tab1, tab2, tab3 = st.tabs(["Intro", "Tables", "Area"])
 
     with tab1:
-        st.header("Intro")
-        st.write("This is intro page..")
+        st.header("SQL queries with Natural Language Processing")
+        st.markdown("Thank you for joining us today! We are Three Point Consulting and we have prepaired a little game for you to play today. We'll tell you more about it shortly, but let's kick things off with a short introduction to Natural Language Processing or NLP in short.")
+        st.subheader("A short introduction to NLP")
+        st.markdown("NLP is a branch of artificial intelligence that focuses on enabling computers to understand, interpret, and generate human language. Many of you may have experienced this technology with OpenAI's ChatGPT. In fact, this game is powered by the same language model as the ChatGPT+, which is GPT-4. NLP's applications include topics, such as chatbots, semantic search, machine translation, document- and sentiment analysis. We don't want to bother you with the mathematical or technical details of language models or NLP in general, but we are more than happy to discuss these topics if you are interested so don't be afraid to ask!")
+        st.subheader("SQL and Geospatial data")
+        st.markdown("You might also be pretty well acquinted with SQL. If you are not, SQL is a declarative language which allows user to search (or query) data from databases. Today we are going to introduce you to quering data using only natural language, so no more annoying joins, subqueries, etc!")
+        st.subheader("Your task")
+        st.markdown("We are running a Supabase instance locally where different geospatial forest data is stored. PLACEHOLDER You are supposed search (or prompt) the largest forest polygon from our database using only natural language. The SQL query which is created from the prompt is displayed on the exercise page.")
 
     with tab2:
         st.header("All tables")
